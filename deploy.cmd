08d186bcd670f894c8686b6310a57fec97b59348
degaart <degaart@users.noreply.github.com>
2025-11-29 18:34:49 +0300

    Fix deploy.cmd when building with MSVC

--- deploy.cmd
+++ deploy.cmd
@@ -7,6 +7,14 @@ IF "%~1"=="" (
 
 SET DESTDIR=%~1
 if "%DESTDIR:~-1%"=="\" set "DESTDIR=%DESTDIR:~0,-1%"
-COPY build\quake2.exe "%DESTDIR%"
-COPY build\ref_gl.dll "%DESTDIR%"
-COPY build\gamex86.dll "%DESTDIR%\baseq2"
+
+if exist build\quake2.exe (
+    COPY build\quake2.exe "%DESTDIR%"
+    COPY build\ref_gl.dll "%DESTDIR%"
+    COPY build\gamex86.dll "%DESTDIR%\baseq2"
+) else (
+    COPY build\Debug\quake2.exe "%DESTDIR%"
+    COPY build\Debug\ref_gl.dll "%DESTDIR%"
+    COPY build\Debug\gamex86.dll "%DESTDIR%\baseq2"
+)
+
